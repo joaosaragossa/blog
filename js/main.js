b2ca70c2a537951cc90840b7358dca9165d180fa
João Paulo Saragossa <joaosaragossa.dev@gmail.com>
2014-11-04 22:46:09 -0200

    Post: Clara Crocodilo

--- js/main.js
+++ js/main.js
@@ -1,40 +1,59 @@
-window.onload = function()
-{
-  // Header height resize
+// Get headers
 
-  var header = document.getElementById('page-header');
-  
+  if (document.getElementById('page-header')) {
+      
+    var header = document.getElementById('page-header');
+  }
+  else if(document.getElementById('post-header')) {
+
+    var header = document.getElementById('post-header');
+  }
+  else if(document.getElementById('tags-header')) {
+
+    var header = document.getElementById('tags-header');
+  }
+
+// Header height resize
+
+  if (header) {
+    setInterval(function()
+    {
+      header.style.height = window.innerHeight + "px";
+    },0);
+  }
+
+// Post header height resize
+
   if (header){
     setInterval(function()
     {
       header.style.height = window.innerHeight + "px";
     },0);
+  }
 
-  };
+// Post resize li
 
-  // Post header height resize
+  if (document.getElementById('post-list').getElementsByTagName('li')) {
 
-  var postHeader = document.getElementById('post-header');
-  
-  if (postHeader){
-    setInterval(function()
-    {
-      postHeader.style.height = window.innerHeight + "px";
-    },0);
+    var posts = document.getElementById('post-list').getElementsByTagName('li');
+    
+    setInterval(function() {
 
-  };
-
-  // Post list items height resize
-
-  var posts = document.getElementById('post-list').getElementsByTagName('li');
-
-  if (posts){
-    setInterval(function()
-    {
-      for (var i = 0; i < posts.length; i++) 
-      {
+      for (var i = 0; i < posts.length; i++) {
+        
         posts[i].style.height = posts[i].clientWidth + "px";
       }
     },0);
-  };
-}
+  }
+
+// Fuid behavior
+
+  if (window.innerWidth > 960) {
+
+    window.onscroll = function() {
+    var speed = 8.0;
+    header.style.backgroundPosition = (-window.pageXOffset / speed) + "px " + (-window.pageYOffset / speed) + "px";
+
+    console.log(window.innerWidth);
+    }
+  }
