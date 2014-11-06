28d877b5620327b4654fade55036e7be860f6665
João Paulo Saragossa <joaosaragossa.dev@gmail.com>
2014-11-06 16:40:42 -0200

    Finally minified .js and css

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    
+    pkg: grunt.file.readJSON("package.json"),
+
+    uglify: {
+      options: {
+        mangle: true
+      },
+      build: {
+        src: "assets/*.js",
+        dest: "js/main.js"
+      }
+    },
+
+    sass: {
+    dist: {
+      options: {
+        style: 'compressed'
+      },
+      files: {
+        'css/main.css': '_sass/all.sass'
+      }
+    }
+  }
+
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.registerTask('default', ["uglify","sass"]);
+
+};
--- _posts/2014-11-03-aquele-que-observa.markdown
+++ _posts/2014-11-03-aquele-que-observa.markdown
@@ -14,5 +14,4 @@ tags:
   <figcaption>Aquele que Observa - Quem?</figcaption>
 </figure>
 
-
 Uma coisa que aprendi com o pintor americano [Jackson Pollock](http://pt.wikipedia.org/wiki/Jackson_Pollock) é que pintura é um jogo entre controle e caos, sendo **caos** os resultados obtidos por fatores não deliberados ou não calculados pelo artista e **controle** o seu oposto, representado pela ação intelectual e cognitiva do criador. O artista que se dá conta disso e domina esse equilíbrio produz uma obra viva, única, cheia de personalidade e talvez até inteligencia.
